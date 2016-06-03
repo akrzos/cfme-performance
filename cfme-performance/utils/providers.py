0b86169b29c9da8d8e4a796d1bd783d4c7fdfa15
Matt Lombana <mjl345@cornell.edu>
2016-06-02 20:59:27 -0400

    Adding the updated ansible/cleanup/README, the reset-all playbook, and the providers.py metrics bug fix

--- cfme-performance/utils/providers.py
+++ cfme-performance/utils/providers.py
@@ -4,6 +4,88 @@ import json
 import requests
 
 
+def get_all_provider_ids():
+    """Returns an integer list of provider ID's via the Rest API"""
+    logger.debug('Retrieving the list of provider ids')
+
+    provider_ids = []
+    provider_response = requests.get(
+        url="https://" + cfme_performance['appliance']['ip_address'] + "/api/providers",
+        auth=(cfme_performance['appliance']['rest_api']['username'],
+              cfme_performance['appliance']['rest_api']['password']),
+        verify=False
+    )
+
+    provider_json = provider_response.json()
+    provider_urls = provider_json['resources']
+    for url in provider_urls:
+        last_slash = url['href'].rfind('/')
+        provider_ids.append(int(url['href'][last_slash + 1:]))
+
+    return provider_ids
+
+
+def get_all_vm_ids():
+    """Returns an integer list of vm ID's via the Rest API"""
+    logger.debug('Retrieving the list of vm ids')
+
+    vm_ids = []
+    vm_response = requests.get(
+        url="https://" + cfme_performance['appliance']['ip_address'] + "/api/vms",
+        auth=(cfme_performance['appliance']['rest_api']['username'],
+              cfme_performance['appliance']['rest_api']['password']),
+        verify=False
+    )
+
+    vm_json = vm_response.json()
+    vm_urls = vm_json['resources']
+    for url in vm_urls:
+        last_slash = url['href'].rfind('/')
+        vm_ids.append(int(url['href'][last_slash + 1:]))
+
+    return vm_ids
+
+
+def get_provider_details(provider_id):
+    """Returns the id, name, and type associated with the provider_id"""
+    logger.debug('Retrieving the provider details for ID: {}'.format(provider_id))
+
+    id_details = {}
+    id_response = requests.get(
+        url="https://" + cfme_performance['appliance']['ip_address'] +
+            "/api/providers/" + str(provider_id),
+        auth=(cfme_performance['appliance']['rest_api']['username'],
+              cfme_performance['appliance']['rest_api']['password']),
+        verify=False
+    )
+    id_json = id_response.json()
+
+    id_details['name'] = str(id_json['name'])
+    id_details['type'] = str(id_json['type'])
+
+    return id_details
+
+
+def get_vm_details(vm_id):
+    """Returns the id, name, and type associated with the vm_id"""
+    logger.debug('Retrieving the vm details for ID: {}'.format(vm_id))
+
+    vmid_details = {}
+    vmid_response = requests.get(
+        url="https://" + cfme_performance['appliance']['ip_address'] +
+            "/api/vms/" + str(vm_id),
+        auth=(cfme_performance['appliance']['rest_api']['username'],
+              cfme_performance['appliance']['rest_api']['password']),
+        verify=False
+    )
+    vmid_json = vmid_response.json()
+
+    vmid_details['name'] = str(vmid_json['name'])
+    vmid_details['type'] = str(vmid_json['type'])
+
+    return vmid_details
+
+
 def add_provider(provider):
     """Adds Provider via the Rest API."""
     logger.debug('Adding Provider: {}, Type: {}'.format(provider['name'], provider['type']))
@@ -21,7 +103,7 @@ def add_provider(provider):
         }]
     }
 
-    if 'metrics credentials' in provider:
+    if 'metrics_credentials' in provider:
         data_dict['resources'][0]['credentials'].append({
             "userid": provider['metrics_credentials']['username'],
             "password": provider['metrics_credentials']['password'],
@@ -49,11 +131,11 @@ def add_providers(providers):
         add_provider(cfme_performance['providers'][provider])
 
 
-def refresh_provider(provider):
-    logger.debug('TODO: Initiate Provider Refresh')
+def refresh_provider(provider_id):
+    logger.debug('Refreshing Provider with id: {}'.format(provider_id))
 
     appliance = cfme_performance['appliance']['ip_address']
-    response = requests.post("https://" + appliance + "/api/providers/" + ID,  # TODO find out ID
+    response = requests.post("https://" + appliance + "/api/providers/" + str(provider_id),
                              data=json.dumps({"action": "refresh"}),
                              auth=(cfme_performance['appliance']['rest_api']['username'],
                                    cfme_performance['appliance']['rest_api']['password']),
@@ -61,18 +143,18 @@ def refresh_provider(provider):
                              headers={"content-type": "application/json"},
                              allow_redirects=False)
 
-    logger.debug('Refreshed Provider: {}, Response: {}'.format(provider['name'], response))
+    logger.debug('Refreshed Provider: {}, Response: {}'.format(provider_id, response))
 
 
 def refresh_provider_host(provider):
     logger.debug('TODO: Initiate Provider Host Refresh')
 
 
-def refresh_provider_vm(provider):
-    logger.debug('TODO: Initiate Provider VM Refresh')
+def refresh_provider_vm(vm_id):
+    logger.debug('Refreshing VM with ID: {}'.format(vm_id))
 
     appliance = cfme_performance['appliance']['ip_address']
-    response = requests.post("https://" + appliance + "/api/vms/",
+    response = requests.post("https://" + appliance + "/api/vms/" + str(vm_id),
                              data=json.dumps({"action": "refresh"}),
                              auth=(cfme_performance['appliance']['rest_api']['username'],
                                    cfme_performance['appliance']['rest_api']['password']),
@@ -80,5 +162,4 @@ def refresh_provider_vm(provider):
                              headers={"content-type": "application/json"},
                              allow_redirects=False)
 
-    logger.debug('The response for refreshing Provider VM: {} is: {}'
-    .format(provider['name'], response.json()))
+    logger.debug('Refreshed VM: {}, Response: {}'.format(vm_id, response))
