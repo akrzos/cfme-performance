99bd38e27f9a2a7a86ce122d615ec3be773aec57
Dennis Metzger <dmetzger@redhat.com>
2016-08-19 15:03:06 -0400

    cfme_perf.sh refacoring and add upgrading pip to resolve build issues with the older version.

--- cfme_perf.sh
+++ cfme_perf.sh
@@ -7,12 +7,37 @@ else
   YUM=yum
 fi
 
-sudo $YUM install -y python-virtualenv gcc postgresql-devel libxml2-devel libxslt-devel zeromq3-devel libcurl-devel redhat-rpm-config  gcc-c++  freetype-devel libffi-dev
+sudo $YUM install -y @development-tools \
+                     python-virtualenv \
+                     gcc \
+                     postgresql-devel \
+                     libxml2-devel \
+                     libxslt-devel \
+                     libcurl-devel \
+                     redhat-rpm-config \
+                     gcc-c++ \
+                     python-virtualenv \
+                     libffi-devel \
+                     libpng \
+                     libpng-devel \
+                     freetype-devel \
+                     freetype-devel \
+                     openssl-devel
+
+echo "### Creating Virtual Environment: .cfme_perf"
 virtualenv .cfme_perf
+
+echo "### Activating .cfme_per"
 echo "export PYTHONPATH='`pwd`'" | tee -a ./.cfme_perf/bin/activate
 echo "export PYTHONDONTWRITEBYTECODE=yes" | tee -a ./.cfme_perf/bin/activate
-
 . ./.cfme_perf/bin/activate
+
+echo "### Upgrading pip"
+pip install --upgrade pip
+
+echo "### Installing Requirements"
 PYCURL_SSL_LIBRARY=nss pip install -Ur ./requirements.txt
+
 echo "Run '. ./.cfme_perf/bin/activate' to load the virtualenv"
 
+exit 0
