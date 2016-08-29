9d1f1ff6223439e9887fb8755e3be0359752476a
Archit Sharma <arcsharm@redhat.com>
2016-08-29 15:49:04 +0530

    update gitignore and yum requirements in setup

--- cfme_perf.sh
+++ cfme_perf.sh
@@ -7,12 +7,17 @@ else
   YUM=yum
 fi
 
-sudo $YUM install -y python-virtualenv gcc postgresql-devel libxml2-devel libxslt-devel zeromq3-devel libcurl-devel redhat-rpm-config  gcc-c++  freetype-devel libffi-dev
-virtualenv .cfme_perf
+sudo $YUM install -y python-virtualenv gcc postgresql-devel libxml2-devel libxslt-devel zeromq3-devel libcurl-devel redhat-rpm-config  gcc-c++ freetype-devel libffi-devel libcurl-devel
+
+if [[ ! -d .cfme_perf ]]; then
+    virtualenv .cfme_perf
+fi
+
 echo "export PYTHONPATH='`pwd`'" | tee -a ./.cfme_perf/bin/activate
 echo "export PYTHONDONTWRITEBYTECODE=yes" | tee -a ./.cfme_perf/bin/activate
 
 . ./.cfme_perf/bin/activate
-PYCURL_SSL_LIBRARY=nss pip install -Ur ./requirements.txt
-echo "Run '. ./.cfme_perf/bin/activate' to load the virtualenv"
 
+PYCURL_SSL_LIBRARY=nss pip install -Ur ./requirements.txt
+
+echo "Run '. ./.cfme_perf/bin/activate' to load the virtualenv"
